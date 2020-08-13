9b0f8dbcdd8105e8346e74689f241e6d87cdd213
Jeff Klenzing <jklenzing@gmail.com>
2020-08-13 17:38:47 -0400

    ENH: add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020, Authors
+# Full license can be found in License.md and AUTHORS.md
+# -----------------------------------------------------------------------------
+
+from setuptools import setup
+
+
+# Run setup
+setup()
