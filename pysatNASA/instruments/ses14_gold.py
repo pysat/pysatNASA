d68564f5e29cd9aadb9f4e6e9fef48fec5829e65
Jeff Klenzing <jklenzing@gmail.com>
2020-12-14 16:48:34 -0500

    STY: generalize data load

--- pysatNASA/instruments/ses14_gold.py
+++ pysatNASA/instruments/ses14_gold.py
@@ -180,15 +180,16 @@ def load(fnames, tag=None, inst_id=None):
     """
 
     data, mdata = load_netcdf4(fnames, pandas_format=pandas_format)
-    # Add time coordinate from scan_start_time
-    data['time'] = ('nscans',
-                    [dt.datetime.strptime(str(val), "b'%Y-%m-%dT%H:%M:%SZ'")
-                     for val in data['scan_start_time'].values])
-    data = data.swap_dims({'nscans': 'time'})
-    data = data.assign_coords({'nlats': data['nlats'],
-                               'nlons': data['nlons'],
-                               'nmask': data['nmask'],
-                               'channel': data['channel'],
-                               'hemisphere': data['hemisphere']})
+    if tag == 'nmax':
+        # Add time coordinate from scan_start_time
+        data['time'] = ('nscans',
+                        [dt.datetime.strptime(str(val), "b'%Y-%m-%dT%H:%M:%SZ'")
+                         for val in data['scan_start_time'].values])
+        data = data.swap_dims({'nscans': 'time'})
+        data = data.assign_coords({'nlats': data['nlats'],
+                                   'nlons': data['nlons'],
+                                   'nmask': data['nmask'],
+                                   'channel': data['channel'],
+                                   'hemisphere': data['hemisphere']})
 
     return data, mdata
