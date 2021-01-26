6d9b17e25f7da1308527a61ed82d5d5aaef55140
Jeff Klenzing <jklenzing@gmail.com>
2021-01-26 12:10:51 -0500

    BUG: rtd setup

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../pysat'))
 # Need to set path before importing pysatNASA
 import pysatNASA
 
