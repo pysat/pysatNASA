5e51f3f53c95685c8bd2a90b91c5e8f6f48c1ea9
Jeff Klenzing <jklenzing@gmail.com>
2021-04-13 11:59:41 -0400

    BUG: fix path

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 import os
 import re
 import sys
-sys.path.insert(0, os.path.abspath('../pysatNASA'))
+sys.path.insert(0, os.path.abspath('..'))
 import pysatNASA
 
 # -- General configuration ------------------------------------------------
