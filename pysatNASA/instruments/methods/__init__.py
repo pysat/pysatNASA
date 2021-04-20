02e262a99f9393fe8b8cbe015b4702929f131c23
Jonathon Smith <36175570+JonathonMSmith@users.noreply.github.com>
2021-04-20 13:32:51 -0400

    Update __init__.py

--- pysatNASA/instruments/methods/__init__.py
+++ pysatNASA/instruments/methods/__init__.py
@@ -3,4 +3,3 @@ from pysatNASA.instruments.methods import cnofs  # noqa F401
 from pysatNASA.instruments.methods import de2  # noqa F401
 from pysatNASA.instruments.methods import icon  # noqa F401
 from pysatNASA.instruments.methods._cdf import CDF  # noqa F401
-
