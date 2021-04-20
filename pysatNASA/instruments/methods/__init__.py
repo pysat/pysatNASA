d295b12439b3efe837db4afd424a65182a9e623d
JonathonMSmith <36175570+JonathonMSmith@users.noreply.github.com>
2021-04-20 13:06:18 -0400

    added _cdf import to init

--- pysatNASA/instruments/methods/__init__.py
+++ pysatNASA/instruments/methods/__init__.py
@@ -2,3 +2,5 @@ from pysatNASA.instruments.methods import cdaweb  # noqa F401
 from pysatNASA.instruments.methods import cnofs  # noqa F401
 from pysatNASA.instruments.methods import de2  # noqa F401
 from pysatNASA.instruments.methods import icon  # noqa F401
+from pysatNASA.instruments.methods._cdf import CDF  # noqa F401
+
