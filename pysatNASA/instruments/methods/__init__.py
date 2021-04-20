51c44962301b8fb738061300b33c3b849fb3f1c9
JonathonMSmith <36175570+JonathonMSmith@users.noreply.github.com>
2021-04-20 14:23:15 -0400

    re organized imports to avoid circular import

--- pysatNASA/instruments/methods/__init__.py
+++ pysatNASA/instruments/methods/__init__.py
@@ -1,6 +1,6 @@
+from pysatNASA.instruments.methods._cdf import CDF  # noqa F401
 from pysatNASA.instruments.methods import cdaweb  # noqa F401
 from pysatNASA.instruments.methods import cnofs  # noqa F401
 from pysatNASA.instruments.methods import de2  # noqa F401
 from pysatNASA.instruments.methods import icon  # noqa F401
-from pysatNASA.instruments.methods._cdf import CDF  # noqa F401
 
