f7d07d9f227c311f52089aa00f47179f6e9aee6d
Jeff Klenzing <jklenzing@gmail.com>
2021-06-01 12:23:29 -0400

    STY: conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -116,32 +116,29 @@ htmlhelp_basename = '{:s}doc'.format(project)
 
 
 # -- Options for LaTeX output ---------------------------------------------
+# The paper size ('letterpaper' or 'a4paper').
+#
+# 'papersize': 'letterpaper',
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
+# The font size ('10pt', '11pt' or '12pt').
+#
+# 'pointsize': '10pt',
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
+# Additional stuff for the LaTeX preamble.
+#
+# 'preamble': '',
 
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
+# Latex figure (float) alignment
+#
+# 'figure_align': 'htbp',
 
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
+latex_elements = {}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, '{:s}.tex'.format(project), title,
-     author, 'manual'),
-]
+latex_documents = [(master_doc, '{:s}.tex'.format(project), title, author,
+                    'manual')]
 
 
 # -- Options for manual page output ---------------------------------------
