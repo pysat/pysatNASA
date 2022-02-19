9e193761940d24b600931d9d90fb90fc3bb49149
Russell Stoneback <rstoneba@utdallas.edu>
2022-02-18 18:48:11 -0600

    BUG: imports

--- pysatNASA/instruments/methods/cdaweb.py
+++ pysatNASA/instruments/methods/cdaweb.py
@@ -22,7 +22,7 @@ try:
     import pysatCDF
     CDF = pysatCDF.CDF
 except ImportError:
-    from pysatNASA.instruments.methods import CDF as CDF
+    CDF = libCDF
 
 
 def load(fnames, tag=None, inst_id=None, file_cadence=dt.timedelta(days=1),
