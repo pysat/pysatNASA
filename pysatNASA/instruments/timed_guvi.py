5449a9a38d5dda0347ba184e13f9543221b4d74e
landsito <luis.navarro.dominguez@gmail.com>
2022-03-30 13:12:27 -0600

    Moving towards downlaoding NETCDF files from versino13 of L1 disk data. Adding timed_guvi in __init__. cdaweb needs improvements to support doy format in fname when downloading

--- pysatNASA/instruments/timed_guvi.py
+++ pysatNASA/instruments/timed_guvi.py
@@ -32,9 +32,9 @@ Example
 ::
 
     import pysat
-    ivm = pysat.Instrument(platform='timed', name='guvi', inst_id='l1c')
-    ivm.download(dt.datetime(2020, 1, 1), dt.datetime(2020, 1, 31))
-    ivm.load(2020, 1)
+    guvi = pysat.Instrument(platform='timed', name='guvi')
+    guvi.download(dt.datetime(2005, 6, 20), dt.datetime(2005, 6, 21))
+    guvi.load(yr=2005,doy=171)
 
 Author
 ------
@@ -46,6 +46,7 @@ import datetime as dt
 import functools
 import pandas as pds
 import warnings
+import pysat
 
 from pysat import logger
 from pysat.instruments.methods import general as mm_gen
@@ -63,7 +64,7 @@ inst_ids = {'': [tag for tag in tags.keys()]}
 # ----------------------------------------------------------------------------
 # Instrument test attributes
 
-_test_dates = {'': {'': dt.datetime(2009, 1, 1)}}
+_test_dates = {'': {'': dt.datetime(2005, 6, 20)}}
 
 # ----------------------------------------------------------------------------
 # Instrument methods
@@ -114,22 +115,75 @@ def clean(self):
 # Use the default CDAWeb and pysat methods
 
 # Set the list_files routine
-fname = 'timed_L1Cdisk_guvi_{year:04d}{month:02d}{day:02d}{hour:02d}{minute:02d}{second:02d}_v{version:02d}.cdf'
+fname = 'TIMED_GUVI_L1C-2-disk-IMG_{year:04d}{day:03d}'\
+        '{hour:02d}{minute:02d}{second:02d}-{end:13d}_'\
+        'REV{rev:06d}_Av{version:02d}-{versionb:02d}r{revision:03d}.nc'
 supported_tags = {'': {'': fname}}
 list_files = functools.partial(mm_gen.list_files,
-                               supported_tags=supported_tags,
-                               file_cadence=pds.DateOffset(hours=1))
-
-# Set the load routine
-load = functools.partial(cdw.load, file_cadence=pds.DateOffset(hours=1))
+                               supported_tags=supported_tags,)
 
 # Set the download routine
-basic_tag = {'remote_dir': ''.join(('/pub/data/timed/guvi/levels_v10/level1c/disk_cdf',
-                                    '/{year:4d}/{month:02d}/')),
+basic_tag = {'remote_dir': ''.join(('/pub/data/timed/guvi/levels_v13/level1c/imaging/',
+                                    '/{year:4d}/{day:03d}/')),
              'fname': fname}
 download_tags = {'': {'': basic_tag}}
 download = functools.partial(cdw.download, supported_tags=download_tags)
 
 # Set the list_remote_files routine
-list_remote_files = functools.partial(cdw.list_remote_files,
-                                      supported_tags=download_tags)
+list_remote_files = functools.partial(cdw.list_remote_files, supported_tags=download_tags)
+
+# Set the load routine
+# load = cdw.load
+# load = functools.partial(cdw.load, flatten_twod=False,file_cadence=dt.timedelta(hours=1))
+def load(fnames, tag=None, inst_id=None):
+    """Load ICON IVM data into `pandas.DataFrame` and `pysat.Meta` objects.
+
+    This routine is called as needed by pysat. It is not intended
+    for direct user interaction.
+
+    Parameters
+    ----------
+    fnames : array-like
+        iterable of filename strings, full path, to data files to be loaded.
+        This input is nominally provided by pysat itself.
+    tag : string
+        tag name used to identify particular data set to be loaded.
+        This input is nominally provided by pysat itself.
+    inst_id : string
+        Satellite ID used to identify particular data set to be loaded.
+        This input is nominally provided by pysat itself.
+    keep_original_names : boolean
+        if True then the names as given in the netCDF ICON file
+        will be used as is. If False, a preamble is removed.
+
+    Returns
+    -------
+    data : pds.DataFrame
+        A pandas DataFrame with data prepared for the pysat.Instrument
+    meta : pysat.Meta
+        Metadata formatted for a pysat.Instrument object.
+
+    Note
+    ----
+    Any additional keyword arguments passed to pysat.Instrument
+    upon instantiation are passed along to this routine.
+
+    Examples
+    --------
+    ::
+
+        inst = pysat.Instrument('icon', 'ivm', inst_id='a', tag='')
+        inst.load(2020, 1)
+
+    """
+
+    labels = {'units': ('Units', str), 'name': ('Long_Name', str),
+              'notes': ('Var_Notes', str), 'desc': ('CatDesc', str),
+              'min_val': ('ValidMin', float),
+              'max_val': ('ValidMax', float), 'fill_val': ('FillVal', float)}
+
+    data, meta = pysat.utils.load_netcdf4(fnames, epoch_name='Epoch',file_format='netcdf4',
+                                          labels=labels)
+
+    return data, meta
+
