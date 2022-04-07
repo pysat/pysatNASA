1c707a0fb70965e6661418a1bb73cb89592f13d3
land <69330209+landsito@users.noreply.github.com>
2022-04-07 11:08:23 -0600

    Update pysatNASA/instruments/__init__.py
    Co-authored-by: Angeline Burrell <aburrell@users.noreply.github.com>

--- pysatNASA/instruments/__init__.py
+++ pysatNASA/instruments/__init__.py
@@ -10,7 +10,7 @@ __all__ = ['cnofs_ivm', 'cnofs_plp', 'cnofs_vefi',
            'formosat1_ivm',
            'icon_euv', 'icon_fuv', 'icon_ivm', 'icon_mighti',
            'iss_fpmu', 'omni_hro', 'ses14_gold',
-           'timed_saber', 'timed_see','timed_guvi']
+           'timed_guvi', 'timed_saber', 'timed_see']
 
 for inst in __all__:
     exec("from pysatNASA.instruments import {x}".format(x=inst))
