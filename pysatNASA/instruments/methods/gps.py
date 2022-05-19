3377c35f560da5242e58588a1b979ca6fb63b3b5
JonathonMSmith <36175570+JonathonMSmith@users.noreply.github.com>
2022-05-19 19:56:31 -0400

    added acknowledgements and references for roti instrument in gps

---
+++ pysatNASA/instruments/methods/gps.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Provides non-instrument specific routines for JPL ROTI data."""
+
+ackn_str = ' '.join(("The GPS Total Electron Content (TEC) data",
+                     "produced by the International Global Navigation",
+                     "Satellite Systems Service (IGS) Ionosphere Working",
+                     "Group is provided through CDAWeb"))
+
+refs = {'mission': ' '.join(('Feltens J, Schaer S (1998) IGS Products for the',
+                   'Ionosphere, IGS Posi- tion Paper. In: Proceedings of the',
+                   'IGS analysis centers workshop, ESOC, Darmstadt, Germany,',
+                   'pp 225–232, 9–11 February')),
+        'roti15min_jpl': ' '.join(('1.	Pi, X., A. J. Mannucci, U. J.',
+                                   'Lindqwister, and C. M. Ho, Monitoring of',
+                                   'global ionospheric irregularities using',
+                                   'the worldwide GPS network, Geophys. Res.',
+                                   'Lett., 24, 2283, 1997. 2.	Pi, X., F. J.',
+                                   'Meyer, K. Chotoo, Anthony Freeman, R. G.',
+                                   'Caton, and C. T. Bridgwood, Impact of',
+                                   'ionospheric scintillation on Spaceborne',
+                                   'SAR observations studied using GNSS,',
+                                   'Proc. ION-GNSS, pp.1998-2006, 2012.'))}
