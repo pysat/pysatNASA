e9aa6e72d8fa063702f9fa939e0d9df262076a93
jklenzing <jklenzing@users.noreply.github.com>
2022-11-03 10:46:15 -0400

    ENH: de2 idm

--- pysatNASA/instruments/__init__.py
+++ pysatNASA/instruments/__init__.py
@@ -5,7 +5,7 @@ Each instrument is contained within a subpackage of this set.
 
 """
 
-__all__ = ['cnofs_ivm', 'cnofs_plp', 'cnofs_vefi',
+__all__ = ['cnofs_ivm', 'cnofs_plp', 'cnofs_vefi', 'de2_idm',
            'de2_lang', 'de2_nacs', 'de2_rpa', 'de2_wats', 'de2_vefi',
            'formosat1_ivm',
            'icon_euv', 'icon_fuv', 'icon_ivm', 'icon_mighti',
