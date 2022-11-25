336b5a8592eb2563c7b85e6770b9d71c208c61da
Jeff Klenzing <19592220+jklenzing@users.noreply.github.com>
2022-11-25 18:57:05 -0500

    Update ace_mag_l2.py

--- pysatNASA/instruments/ace_mag_l2.py
+++ pysatNASA/instruments/ace_mag_l2.py
@@ -45,7 +45,7 @@ from pysatNASA.instruments.methods import general as mm_nasa
 # Instrument attributes
 
 platform = 'ace'
-name = 'mag'
+name = 'mag_l2'
 tags = {'': ''}
 inst_ids = {id: [''] for id in ['h0', 'h1', 'h2', 'h3', 'k0', 'k1', 'k2']}
 pandas_format = False
