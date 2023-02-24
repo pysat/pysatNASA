a1889e4635c42e769bb89c852bd36e53f92bb613
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2023-02-24 17:05:26 -0500

    ENH: added DMSP SSUSI instrument
    Added a DMSP SSUSI instrument, currently only handling one of the available data products from this instrument.

---
+++ pysatNASA/instruments/methods/dmsp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Provides non-instrument specific routines for the DMSP data."""
+
+ackn_str = "".join(["This Defense Meteorological Satellite Program (DMSP) ",
+                    "satellite data is provided through CDAWeb"])
+
+refs = {'ssusi': ' '.join(('Larry J. Paxton, Daniel Morrison, Yongliang Zhang,',
+                           ' Hyosub Kil, Brian Wolven, Bernard S. Ogorzalek, ',
+                           'David C. Humm, and Ching-I. Meng "Validation of ',
+                           'remote sensing products produced by the Special ',
+                           'Sensor Ultraviolet Scanning Imager (SSUSI): a far ',
+                           'UV-imaging spectrograph on DMSP F-16", Proc. SPIE ',
+                           '4485, Optical Spectroscopic Techniques, Remote ',
+                           'Sensing, and Instrumentation for Atmospheric and ',
+                           'Space Research IV, (30 January 2002); ',
+                           'doi:10.1117/12.454268'))}
