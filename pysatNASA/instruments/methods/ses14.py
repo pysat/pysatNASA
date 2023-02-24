3277e735b3259d2173174aa9702b94e6a5ff350e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2023-02-24 18:56:15 -0500

    STY: renamed gold to ses14
    Renamed the gold module ses14 to maintain a consistent nomenclature for the platform modules.

--- pysatNASA/instruments/methods/ses14.py
+++ pysatNASA/instruments/methods/ses14.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Provides non-instrument specific routines for GOLD data."""
+"""Provides non-instrument specific routines for SES14 instruments."""
 
 ack_str = ' '.join(('This is a data product from the NASA Global-scale',
                     'Observations of the Limb and Disk (GOLD) mission, an',
@@ -18,7 +18,7 @@ ack_str = ' '.join(('This is a data product from the NASA Global-scale',
                     'Investigator Dr. Richard Eastes and the party',
                     'directly responsible for the data product and the',
                     'NASA Explorers Project Office.'))
-ref_str = ' '.join(('Eastes, R.W., McClintock, W.E., Burns, A.G. et al.',
-                    'The Global-Scale Observations of the Limb and Disk',
-                    '(GOLD) Mission. Space Sci Rev 212, 383–408 (2017).',
-                    'https://doi.org/10.1007/s11214-017-0392-2'))
+refs = {'gold': ' '.join(('Eastes, R.W., McClintock, W.E., Burns, A.G. et',
+                          'al., The Global-Scale Observations of the Limb',
+                          'and Disk (GOLD) Mission. Space Sci Rev 212,',
+                          '383–408 (2017). doi:10.1007/s11214-017-0392-2'))}
