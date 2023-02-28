8d63b05016172dd5a4a00f446c93bd565001f35e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2023-02-27 22:31:54 -0500

    BUG: fixed spaces
    Removed the extra space when joining.

--- pysatNASA/instruments/methods/dmsp.py
+++ pysatNASA/instruments/methods/dmsp.py
@@ -4,13 +4,13 @@
 ackn_str = "".join(["This Defense Meteorological Satellite Program (DMSP) ",
                     "satellite data is provided through CDAWeb"])
 
-refs = {'ssusi': ' '.join(('Larry J. Paxton, Daniel Morrison, Yongliang Zhang,',
-                           ' Hyosub Kil, Brian Wolven, Bernard S. Ogorzalek, ',
-                           'David C. Humm, and Ching-I. Meng "Validation of ',
-                           'remote sensing products produced by the Special ',
-                           'Sensor Ultraviolet Scanning Imager (SSUSI): a far ',
-                           'UV-imaging spectrograph on DMSP F-16", Proc. SPIE ',
-                           '4485, Optical Spectroscopic Techniques, Remote ',
-                           'Sensing, and Instrumentation for Atmospheric and ',
-                           'Space Research IV, (30 January 2002); ',
-                           'doi:10.1117/12.454268'))}
+refs = {'ssusi': ''.join(('Larry J. Paxton, Daniel Morrison, Yongliang Zhang,',
+                          ' Hyosub Kil, Brian Wolven, Bernard S. Ogorzalek, ',
+                          'David C. Humm, and Ching-I. Meng "Validation of ',
+                          'remote sensing products produced by the Special ',
+                          'Sensor Ultraviolet Scanning Imager (SSUSI): a far ',
+                          'UV-imaging spectrograph on DMSP F-16", Proc. SPIE ',
+                          '4485, Optical Spectroscopic Techniques, Remote ',
+                          'Sensing, and Instrumentation for Atmospheric and ',
+                          'Space Research IV, (30 January 2002); ',
+                          'doi:10.1117/12.454268'))}
