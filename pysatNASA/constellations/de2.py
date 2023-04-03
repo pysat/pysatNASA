aadd5f789030c6cfd94a3f190027d609c2dc9f9c
Jeff Klenzing <jklenzing@users.noreply.github.com>
2023-04-03 16:51:00 -0400

    ENH: update DE2 constellation

--- pysatNASA/constellations/de2.py
+++ pysatNASA/constellations/de2.py
@@ -5,9 +5,11 @@ import pysat
 from pysatNASA import instruments
 
 
+fpi = pysat.Instrument(inst_module=instruments.de2_fpi)
 lang = pysat.Instrument(inst_module=instruments.de2_lang)
 nacs = pysat.Instrument(inst_module=instruments.de2_nacs)
 rpa = pysat.Instrument(inst_module=instruments.de2_rpa)
 wats = pysat.Instrument(inst_module=instruments.de2_wats)
+vefi = pysat.Instrument(inst_module=instruments.de2_vefi)
 
-instruments = [lang, nacs, rpa, wats]
+instruments = [fpi, lang, nacs, rpa, wats, vefi]
