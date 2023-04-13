0712ffea2d703153ee05e5485f1ece4affc89797
land <69330209+landsito@users.noreply.github.com>
2023-04-13 11:07:48 -0600

    Update pysatNASA/instruments/ses14_gold.py
    Co-authored-by: Jonathon Smith <36175570+JonathonMSmith@users.noreply.github.com>

--- pysatNASA/instruments/ses14_gold.py
+++ pysatNASA/instruments/ses14_gold.py
@@ -40,7 +40,6 @@ Examples
 import datetime as dt
 import functools
 import numpy as np
-import xarray as xr
 
 from pysat.instruments.methods import general as ps_gen
 from pysat.utils.io import load_netcdf
