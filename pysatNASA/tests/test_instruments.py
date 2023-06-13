39373f61959e91ee70359d1e1d48e55fe3669dc3
Jonathon Smith <36175570+JonathonMSmith@users.noreply.github.com>
2023-06-13 12:38:07 -0400

    Update pysatNASA/tests/test_instruments.py
    Co-authored-by: Jeff Klenzing <19592220+jklenzing@users.noreply.github.com>

--- pysatNASA/tests/test_instruments.py
+++ pysatNASA/tests/test_instruments.py
@@ -98,9 +98,6 @@ class TestInstruments(clslib.InstLibTests):
 
     @pytest.mark.second
     @pytest.mark.parametrize("inst_dict", instruments['cdf'])
-    @pytest.mark.skipif(cdflib_only,
-                        reason=" ".join(("Additional load tests not required",
-                                         "when pysatCDF not installed")))
     def test_meta_header(self, inst_dict):
         """Test that instruments have header level metadata attached.
 
