f0ed7c506f5f2dbedf0d80d88f1ab04361fcf0ff
Jonathon Smith <36175570+JonathonMSmith@users.noreply.github.com>
2023-06-14 15:04:17 -0400

    removed TODO related to kwargs

--- pysatNASA/instruments/methods/ace.py
+++ pysatNASA/instruments/methods/ace.py
@@ -34,8 +34,6 @@ refs = {'mission': ' '.join(('Stone, E., Frandsen, A., Mewaldt, R. et al.',
         }
 
 
-# TODO(https://github.com/pysat/pysat/issues/1020): **kwargs can be removed
-# when header level meta is tested in version 3.3.0+ the core pysat
 def load(fnames, tag='', inst_id='', to_pandas=False, **kwargs):
     """Load ACE data via xarray and convert to pandas if needed.
 
