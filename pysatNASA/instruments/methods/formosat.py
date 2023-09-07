d9f69e5bd68ae7e880bd5132d5c7d248c07bd118
jklenzing <jklenzing@users.noreply.github.com>
2023-09-07 10:30:27 -0400

    STY: use standard init

---
+++ pysatNASA/instruments/methods/formosat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Provides non-instrument specific routines for C/NOFS data."""
+
+ackn_str = ' '.join(('Data provided through NASA CDAWeb Key Parameters -',
+                     'Shin-Yi Su (Institute of Space Science, National Central',
+                     'University, Taiwan, R.O.C.)'))
+refs = {'ivm': ' '.join(('Yeh, H.C., S.‐Y. Su, Y.C. Yeh, J.M. Wu, R. A.',
+                         'Heelis, and B. J. Holt, Scientific mission of the',
+                         'IPEI payload on board ROCSAT‐1, Terr. Atmos. Ocean.',
+                         'Sci., 9, suppl., 1999a.\n',
+                         'Yeh, H.C., S.‐Y. Su, R.A. Heelis, and J.M. Wu, The',
+                         'ROCSAT‐1 IPEI preliminary results, Vertical ion',
+                         'drift statistics, Terr. Atmos. Ocean. Sci., 10, 805,',
+                         '1999b.'))}
