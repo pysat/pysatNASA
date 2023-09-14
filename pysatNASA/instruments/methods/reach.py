a67c42150d6898f79450a92c478d0584c853bda8
Jeff Klenzing <jklenzing@users.noreply.github.com>
2023-09-14 17:41:28 -0400

    ENH: add reach dosimeter

---
+++ pysatNASA/instruments/methods/reach.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Provides non-instrument specific routines for REACH data."""
+
+ackn_str = ' '.join(("Insert ackn"))
+
+refs = {'dosimeter': ' '.join(('Insert ref'))
+        }
