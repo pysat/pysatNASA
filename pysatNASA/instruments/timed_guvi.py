33ed75f64f384f0c6035cdd302dc9d703f6838ea
Jeff Klenzing <jklenzing@users.noreply.github.com>
2024-03-05 16:54:12 -0500

    MAINT: remove new test flag from timed_guvi

--- pysatNASA/instruments/timed_guvi.py
+++ pysatNASA/instruments/timed_guvi.py
@@ -95,9 +95,6 @@ _test_dates = {
 _test_load_opt = {iid: {tag: {'combine_times': True}
                         for tag in inst_ids[iid]}
                   for iid in ['high_res', 'low_res']}
-# TODO(#218): Remove when compliant with multi-day load tests
-_new_tests = {iid: {tag: False for tag in inst_ids[iid]}
-              for iid in ['high_res', 'low_res']}
 _clean_warn = {inst_id: {tag: mm_nasa.clean_warnings
                          for tag in inst_ids[inst_id] if tag != 'sdr-imaging'}
                for inst_id in inst_ids.keys()}
