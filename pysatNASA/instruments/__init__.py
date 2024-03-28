6d6086fc40f4030def675d33df94c06fec5a6436
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2024-03-28 12:14:38 -0400

    DOC: updated instrument with pub release
    Added the NRL pub release to the instrument sub-module files.

--- pysatNASA/instruments/__init__.py
+++ pysatNASA/instruments/__init__.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Full license can be found in License.md
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.3986131
+#
+# DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
+# unlimited.
+# ----------------------------------------------------------------------------
 """Collection of instruments for the pysatNASA library.
 
 Each instrument is contained within a subpackage of this set.
--- pysatNASA/instruments/reach_dosimeter.py
+++ pysatNASA/instruments/reach_dosimeter.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Full license can be found in License.md
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.3986131
+#
+# DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
+# unlimited.
+# ----------------------------------------------------------------------------
 """The REACH dosimeter instrument.
 
 Supports the dosimeter instrument on the Responsive Environmental Assessment
